bd282f74b8c7756f0572cd6e822a90966c82f080
Laza Bogdan <laza.bogdan@macro-net.ro>
2018-11-22 15:27:31 +0200

    initial commit

---
+++ src/AppIcon.vue
@@ -0,0 +1,34 @@
+<style scoped="" lang="scss">
+  .icon {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  .title {
+    max-width: 112px;
+    white-space: nowrap; 
+    overflow: hidden;
+    text-overflow: ellipsis;
+    padding: 0 .35rem;
+  }
+  .icon-40pt {
+    font-size: 40px !important;
+  }
+</style>
+
+<script>
+  export default {
+    props: {
+      icon: {
+        type: Object,
+        default() {
+          return {
+            name: 'timer',
+            className: 'timer'
+          }
+        }
+      }
+    }
+  }
+</script>
---
+++ src/app-icons.js
@@ -0,0 +1,8 @@
+import AppIconsApp from './components/App.vue'
+
+new Vue({
+  el: '#app-icons',
+  components: {
+    AppIconsApp
+  }
+})
